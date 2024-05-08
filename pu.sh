2440f8e02d4a2c50febd68805531a6d8dcc84557
John Wickerson <j.wickerson@imperial.ac.uk>
2024-05-08 13:55:22 +0100

    tweaking pushing script

--- pu.sh
+++ pu.sh
@@ -2,6 +2,24 @@ git checkout modules.dot
 git checkout modules.pdf
 git checkout modules_neato.pdf
 git checkout modules_radial.pdf
+git checkout modules_analogue.pdf
+git checkout modules_biomedical.pdf
+git checkout modules_comms.pdf
+git checkout modules_compsci.pdf
+git checkout modules_control.pdf
+git checkout modules_devices.pdf
+git checkout modules_digital.pdf
+git checkout modules_fields.pdf
+git checkout modules_image_audio.pdf
+git checkout modules_maths.pdf
+git checkout modules_ml.pdf
+git checkout modules_optimisation.pdf
+git checkout modules_power_electronics.pdf
+git checkout modules_power_systems.pdf
+git checkout modules_prof.pdf
+git checkout modules_robotics.pdf
+git checkout modules_signals.pdf
 git checkout modules_with_root.dot
+git checkout modules
 git pull --rebase
 git push
