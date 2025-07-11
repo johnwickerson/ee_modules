037d33505d63654b52f72a56d0414e5972528087
John Wickerson <johnwickerson@users.noreply.github.com>
2025-07-11 11:42:57 +0100

    adding Adria's colour coding

--- buildtable.ml
+++ buildtable.ml
@@ -29,46 +29,57 @@ let term_index_of_module m =
   | "autumn", "summer" -> 5
   | _, _ -> failwith "invalid term(s) provided"
                      
-let print_module rowspan y m =
-  let name = name_of_module m in
-  let code = code_of_module m in
+let print_module y m =
+  let name       = name_of_module m in
+  let code       = code_of_module m in
   let start_term = startterm_of_module m in
-  let end_term = endterm_of_module m in
+  let end_term   = endterm_of_module m in
   let term =
-    if start_term = end_term then
-      start_term
-    else
-      sprintf "%s&dash;%s" start_term end_term
+    if start_term = end_term then start_term
+    else sprintf "%s&dash;%s" start_term end_term
   in
   let stream = stream_of_module m in
-  let is_eee, is_eie, is_eeem = match stream with
-    | "both" -> true, true, true
-    | "eie" -> false, true, false
-    | "eee" -> true, false, true
-    | "management" -> false, false, true
-    | _ -> failwith "invalid stream"
+
+  (* pick row colour by stream *)
+  let bg_colour = match stream with
+    | "both"       -> "#ddebf7"
+    | "eee"        -> "#e2efda"
+    | "eie"        -> "#ededed"
+    | "management" -> "#daefe9"
+    | _            -> "transparent"
   in
+
+  (* pick tick-mark flags as before *)
+  let is_eee, is_eie, is_eeem = match stream with
+    | "both"       -> true,  true,  true
+    | "eie"        -> false, true,  false
+    | "eee"        -> true,  false, true
+    | "management" -> false, false, true
+    | _            -> failwith ("invalid stream: " ^ stream)
+  in
+
   let major_themes = major_themes_of_module m in
   let minor_themes = minor_themes_of_module m in
-  printf "  <tr>\n";
-  (match rowspan with
-  | None -> ()
-  | Some n -> printf "    <td rowspan=\"%d\">year %d</td>" n y);
+
+  (* emit row with background colour *)
+  printf "  <tr style=\"background-color: %s;\">\n" bg_colour;
+  printf "    <td>year %d</td>\n" y;
   printf "    <td>%s</td>\n" term;
   printf "    <td>%s</td>\n" name;
-  printf "    <td>%s</td>\n" (if is_eee then "&check;" else "");
-  printf "    <td>%s</td>\n" (if is_eie then "&check;" else "");
+  printf "    <td>%s</td>\n" (if is_eee  then "&check;" else "");
+  printf "    <td>%s</td>\n" (if is_eie  then "&check;" else "");
   printf "    <td>%s</td>\n" (if is_eeem then "&check;" else "");
   printf "    <td>%s</td>\n" code;
+
   List.iter (fun t ->
-      if List.mem t major_themes then
-        printf "    <td class=\"major\">M</td>\n"
-      else if List.mem t minor_themes then
-        printf "    <td class=\"minor\">m</td>\n"
-      else
-        printf "<td></td>\n"
-    )
-    all_themes;
+    if List.mem t major_themes then
+      printf "    <td class=\"major\">M</td>\n"
+    else if List.mem t minor_themes then
+      printf "    <td class=\"minor\">m</td>\n"
+    else
+      printf "    <td></td>\n"
+  ) all_themes;
+
   printf "  </tr>\n"
                      
 let main () =
@@ -87,6 +98,10 @@ let main () =
   printf "td.minor {\n";
   printf "  background-color: #99d8c9;\n";
   printf "}\n";
+  printf "td.stream-common { background-color: #ddebf7; }\n";  (* both / common *)
+  printf "td.stream-eee    { background-color: #e2efda; }\n";
+  printf "td.stream-eie    { background-color: #ededed; }\n";
+  printf "td.stream-mgmt   { background-color: #daefe9; }\n";
   printf "th\n";
   printf "{\n";
   printf "  vertical-align: bottom;\n";
@@ -113,6 +128,22 @@ let main () =
   printf "    <td class=\"minor\">m</td>\n";
   printf "    <td>minor theme</td>\n";
   printf "  </tr>\n";
+  printf "  <tr>\n";
+  printf "    <td class=\"stream-common\">&nbsp;</td>\n";
+  printf "    <td>common modules</td>\n";
+  printf "  </tr>\n";
+  printf "  <tr>\n";
+  printf "    <td class=\"stream-eee\">&nbsp;</td>\n";
+  printf "    <td>EEE modules</td>\n";
+  printf "  </tr>\n";
+  printf "  <tr>\n";
+  printf "    <td class=\"stream-eie\">&nbsp;</td>\n";
+  printf "    <td>EIE modules</td>\n";
+  printf "  </tr>\n";
+  printf "  <tr>\n";
+  printf "    <td class=\"stream-mgmt\">&nbsp;</td>\n";
+  printf "    <td>management modules</td>\n";
+  printf "  </tr>\n";
   printf "</table>\n";
   printf "<table>\n";
   printf "  <tr>\n";
@@ -125,16 +156,41 @@ let main () =
       printf "    <th><span>%s</span></th>\n" (description_of t))
     all_themes;
   printf "  </tr>\n";
-  let yml_top = Yaml_unix.of_file_exn Fpath.(v "modules.yml") in
+  let read_yaml_file filename =
+    let ch = open_in filename in
+    let len = in_channel_length ch in
+    let buf = really_input_string ch len in
+    close_in ch;
+    match Yaml.of_string buf with
+    | Ok y -> y
+    | Error (`Msg m) -> failwith ("YAML parse error: " ^ m)
+  in
+  let yml_top = read_yaml_file "modules.yml" in
   let all_modules = as_yaml_dictionary yml_top in
   let year_of_module_is y m = year_of_module m = float_of_int y in
   let modules_by_year y = List.filter (year_of_module_is y) all_modules in
   for y = 1 to 4 do
     let ms = modules_by_year y in
-    let cmp m1 m2 = compare (term_index_of_module m1) (term_index_of_module m2) in
+    let stream_order stream =
+      match stream with
+      | "both" -> 0
+      | "eee" -> 1
+      | "eie" -> 2
+      | "management" -> 3
+      | _ -> 4  (* unknown stream goes last *)
+    in
+    let cmp m1 m2 =
+      let c1 = compare (term_index_of_module m1) (term_index_of_module m2) in
+      if c1 <> 0 then c1
+      else
+        let c2 = compare (stream_order (stream_of_module m1)) (stream_order (stream_of_module m2)) in
+       if c2 <> 0 then c2
+       else
+         compare (name_of_module m1) (name_of_module m2)
+    in
     let ms = List.sort cmp ms in
-    print_module (Some (List.length ms)) y (List.hd ms);
-    List.iter (print_module None y) (List.tl ms);
+    let ms = List.sort cmp ms in
+    List.iter (print_module y) ms;    
     printf "\n"
   done;
   printf "</table>\n";
